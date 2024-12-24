7193208dfbdcadef81c0710a2eefe0959d99bfdd
8eastwood <m.fishborn@yandex.ru>
2024-12-24 06:31:29 +0300

    init

--- Assets/Assets/Scripts/Mover.cs
+++ Assets/Assets/Scripts/Mover.cs
@@ -5,18 +5,21 @@ public class Mover : MonoBehaviour
     private const string Horizontal = "Horizontal";
 
     [SerializeField] private Rigidbody2D _rigidBody;
-    [SerializeField] private Transform _groundCheck;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private Transform _sprite;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _speed = 4f;
-    
-    public readonly int Speed = Animator.StringToHash(nameof(Speed));
+
     public readonly int Jumping = Animator.StringToHash(nameof(Jumping));
+    public readonly int Speed = Animator.StringToHash(nameof(Speed));
+    private Quaternion _inverseRotation = Quaternion.Euler(0, 180, 0);
     private KeyCode _jumpKey = KeyCode.Space;
     private float _horizontalMove;
     private float _jumpingPower = 8f;
-    private bool _isFacingCorrect = true;
+    private float _radius = 0.2f;
     private bool _isJump;
+    private bool _isFacingCorrect = true;
 
     private void FixedUpdate()
     {
@@ -26,7 +29,13 @@ public class Mover : MonoBehaviour
         {
             _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _jumpingPower);
             _animator.SetBool(Jumping, true);
-            _isJump = false;
+            Debug.Log("ןנûד");
+
+            if (IsGrounded())
+            {
+                _isJump = false;
+
+            }
         }
 
         if (_isJump == false)
@@ -40,7 +49,7 @@ public class Mover : MonoBehaviour
         _horizontalMove = Input.GetAxisRaw(Horizontal);
         _animator.SetFloat(Speed, Mathf.Abs(_horizontalMove));
 
-        if (Input.GetKeyDown(_jumpKey) && IsGrounded())  
+        if (Input.GetKeyDown(_jumpKey) && IsGrounded())
         {
             _isJump = true;
         }
@@ -50,19 +59,18 @@ public class Mover : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
+        return Physics2D.OverlapCircle(_groundCheck.position, _radius, _groundLayer);
     }
 
     private void FlipSide()
     {
-        float sideChanger = -1f;
-
-        if (_isFacingCorrect && _horizontalMove < 0f || !_isFacingCorrect && _horizontalMove > 0f)
+        if (_horizontalMove < 0f)
         {
-            _isFacingCorrect = !_isFacingCorrect;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= sideChanger;
-            transform.localScale = localScale;
+            _sprite.rotation = _inverseRotation;
+        }
+        else if (_horizontalMove > 0f)
+        {
+            _sprite.rotation = Quaternion.identity;
         }
     }
 }
